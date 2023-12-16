8f0902025573265cbf83dd8715ae1a857d4a960a
Ken <kenylefou@hotmail.fr>
2023-12-16 13:14:28 +0100

    [resources] Add some

---
+++ resources/launcher-movies.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+/home/izquierdo/dev/phone_camera_import/build/phone_camera_import -s /media/izquierdo/Famille/unsorted -d /media/izquierdo/Famille/Videos -m --remove-copied &
+
---
+++ resources/launcher-pictures.sh
@@ -0,0 +1,4 @@
+#!/bin/env bash
+
+/home/izquierdo/dev/phone_camera_import/build/phone_camera_import -s /media/izquierdo/Famille/unsorted -d /media/izquierdo/Famille/Photos -p --remove-copied &
+
